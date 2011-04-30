4a5e95b7ee4323d64edfaf0c44eb461c67b7f4ee
Nicholas Bruning <nicholas@bruning.com.au>
2011-04-30 17:22:09 +1000

    Updated button group to properly support wrapper tag option

--- lib/css3buttons/button_group.rb
+++ lib/css3buttons/button_group.rb
@@ -11,13 +11,13 @@ module Css3buttons
     end
 
     def render(&block)
-      html_options = @options
+      html_options = @options.clone
       html_options.delete(:wrapper_tag)
+      html_options.delete(:minor)
       html_options[:class] ||= ''
       html_options[:class] = (html_options[:class].split(" ") + ['button-group']).join(" ")
       html_options[:class] = (html_options[:class].split(" ") + ['minor-group']).join(" ") if @options[:minor]
-      html_options.delete(:minor)
-      content_tag(:div, @template.capture(&block), html_options) if block_given?
+      content_tag(@options[:wrapper_tag], @template.capture(&block), html_options) if block_given?
     end
   end
 end
