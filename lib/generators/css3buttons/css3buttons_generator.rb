e496814df7a378efdd9b42c3faa7c7e69652db47
Nicholas Bruning <nicholas@bruning.com.au>
2011-06-04 16:47:06 +1000

    Removed need for gsub in generator

--- lib/generators/css3buttons/css3buttons_generator.rb
+++ lib/generators/css3buttons/css3buttons_generator.rb
@@ -6,6 +6,5 @@ class Css3buttonsGenerator < Rails::Generators::Base
     copy_file "public/images/gh-icons.png", "public/images/css3buttons/css3-github-buttons-icons.png"
     copy_file "public/stylesheets/css3-github-buttons.css", "public/stylesheets/css3buttons/css3-github-buttons.css"
     copy_file "public/stylesheets/reset.css", "public/stylesheets/css3buttons/reset.css"
-    gsub_file "public/stylesheets/css3buttons/css3-github-buttons.css", /\.\.\/images/, "/images/css3buttons"
   end
 end
