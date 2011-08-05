7e3a0d2485a4675cccdb24a2d5bec031a0d64d75
Nicholas Bruning <nicholas@bruning.com.au>
2011-08-05 15:54:13 +1000

    Removed engine

--- lib/css3buttons.rb
+++ lib/css3buttons.rb
@@ -1,11 +1,7 @@
 require 'action_controller'
 require 'action_view'
 
-module Css3buttons
-  if ::Rails.version >= "3.1"
-    require "css3buttons/engine"
-  end
-  
+module Css3buttons 
   module Helpers
     autoload :ButtonHelper, 'css3buttons/helpers/button_helper'
   end
