bedce0d9f1912cb967855901d6f2c30688c0c4b1
grafikchaos <josh@grafikchaos.com>
2012-01-02 13:27:11 -0600

    add html_safe method to the helper's content

--- lib/css3buttons/helpers/split_button_helper.rb
+++ lib/css3buttons/helpers/split_button_helper.rb
@@ -19,7 +19,7 @@ module Css3buttons
         split_btn = create_split_button(links, options)
 
         # return the final split button + wrapping element
-        content_tag(options[:wrapper_tag], split_btn, html_options)
+        content_tag(options[:wrapper_tag], split_btn.html_safe, html_options)
       end
       
       # extracts the first link from the array and 
@@ -28,7 +28,7 @@ module Css3buttons
       def create_split_button(links, options)
         main_action = links.shift 
         dropdown    = create_dropdown(links, options)
-        content_tag(:ul, content_tag(:li, main_action, :class => 'split main') + dropdown, :class => 'split-menu-wrap')
+        content_tag(:ul, content_tag(:li, main_action.html_safe, :class => 'split main') + dropdown.html_safe, :class => 'split-menu-wrap')
       end
       
       # creates the drop down's button + menu
@@ -39,13 +39,13 @@ module Css3buttons
         dropdown_link_classes += " pill" if options[:pill]
         dropdown_link = link_to "", "javascript:void(0);", :class => dropdown_link_classes
 
-        content_tag(:li,  dropdown_link.html_safe + menu, :class => 'split dropdown')
+        content_tag(:li, dropdown_link.html_safe + menu.html_safe, :class => 'split dropdown')
       end
       
       # wrap the remaining links in an <li> tag
       # and insert it into a <ul> element
       def create_dropdown_menu(links)
-        list = links.map { |link| content_tag(:li, link, :class => 'split-menu-item') }
+        list = links.map { |link| content_tag(:li, link.html_safe, :class => 'split-menu-item') }
         content_tag(:ul, list.join("\n").html_safe, :class => 'split-menu')
       end
     end
